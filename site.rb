b2be6df54597ad488e71b2be6264239556026a3b
devin whall <devin.whall100@gmail.com>
2016-05-14 05:57:05 +0000

    Add 'italian' route

--- site.rb
+++ site.rb
@@ -2,11 +2,6 @@ require 'bundler/setup'
 require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
-get '/' do
+get '/italian' do
   <h1> Something here, remember to close it </h1>
-<h2> This will be a medium size header </h2>
-<h3> This will we a small size header </h3>
-<p> And this will be a paragraph </p>
-<img src="http://placekitten.com/g/200/200" />
-<a href="http://twitter.com/yourtwittername"> My twitter! </a>
 end
