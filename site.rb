af767ced04489aeb39b6d2c697d0b6e164a39827
devin whall <devin.whall100@gmail.com>
2016-05-14 05:37:24 +0000

    Add 'italian' route

--- site.rb
+++ site.rb
@@ -3,5 +3,7 @@ require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
-  "Hello World!"
+  "Hello World2!"
+  <h1> Something here, remeber to close it </h1>
 end
+<h1> Something here, remeber to close it </h1>
