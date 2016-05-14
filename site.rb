ed95be894ea34550787c3ac28c38c47e6ffb7ef3
devin whall <devin.whall100@gmail.com>
2016-05-14 05:53:43 +0000

    Add 'italian' route

--- site.rb
+++ site.rb
@@ -3,7 +3,10 @@ require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
-  "Hello World2!"
-  <h1> Something here, remeber to close it </h1>
+  <h1> Something here, remember to close it </h1>
+<h2> This will be a medium size header </h2>
+<h3> This will we a small size header </h3>
+<p> And this will be a paragraph </p>
+<img src="http://placekitten.com/g/200/200" />
+<a href="http://twitter.com/yourtwittername"> My twitter! </a>
 end
-<h1> Something here, remeber to close it </h1>
